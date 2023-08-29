8ce4359bb385a4df0535e8f3f1677f16945bdaa1
Skully <rickbaas05@icloud.com>
2023-08-29 14:54:03 +0200

    Contact page

--- real/subpages/contact/process_form.php
+++ real/subpages/contact/process_form.php
@@ -8,8 +8,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $subject = "New Contact Form Submission from $name";
 
-    $email_body = "Name: $name\n";
-    $email_body .= "Email: $email\n";
+    $email_body = "Name: \n$name\n";
+    $email_body .= "Email: \n$email\n";
     $email_body .= "Message:\n$message";
 
     if (mail($to, $subject, $email_body)) {
